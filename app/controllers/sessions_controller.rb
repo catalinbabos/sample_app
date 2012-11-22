850c6b6d36a1506569249eeede9d6d0dd43440cf
Catalin Babos <catalin.babos@softvision.ro>
2012-11-22 14:15:15 +0200

    Handling invalid singup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.authenticate(params[:user][:email], params[:user][:password])
+  	if user.nil?
+  	  flash.now[:error] = "Invalid email / password combination."	
+  	  render "new"
+  	else
+      # Handle sign in
+  	end
   end
 
   def destroy
